180bda8c97fa1645e4bf2d27961e377ad6cdf9ea
Jalal Amin <37778928+jalalamin@users.noreply.github.com>
2019-04-28 16:09:51 +0600

    file uploaded
    file uploaded

---
+++ js/script.js
@@ -0,0 +1,60 @@
+//Preloader
+function myPreloader() {
+    var preloader = document.getElementById("loding");
+    preloader.style.display = "none";
+}
+
+
+// Course
+function myFunction1() {
+    document.getElementById("elements_one_js").style.display = "block";
+    document.getElementById("elements_two_js").style.display = "none";
+    document.getElementById("elements_three_js").style.display = "none";
+    document.getElementById("elements_four_js").style.display = "none";
+    document.getElementById("elements_five_js").style.display = "none";
+}
+
+
+function myFunction() {
+    document.getElementById("elements_two_js").style.display = "block";
+    document.getElementById("elements_one_js").style.display = "none";
+    document.getElementById("elements_three_js").style.display = "none";
+    document.getElementById("elements_four_js").style.display = "none";
+    document.getElementById("elements_five_js").style.display = "none";
+}
+
+
+function myFunction2() {
+    document.getElementById("elements_three_js").style.display = "block";
+    document.getElementById("elements_one_js").style.display = "none";
+    document.getElementById("elements_two_js").style.display = "none";
+    document.getElementById("elements_four_js").style.display = "none";
+    document.getElementById("elements_five_js").style.display = "none";
+}
+
+
+function myFunction3() {
+    document.getElementById("elements_four_js").style.display = "block";
+    document.getElementById("elements_one_js").style.display = "none";
+    document.getElementById("elements_two_js").style.display = "none";
+    document.getElementById("elements_three_js").style.display = "none";
+    document.getElementById("elements_five_js").style.display = "none";
+}
+
+
+function myFunction4() {
+    document.getElementById("elements_five_js").style.display = "block";
+    document.getElementById("elements_one_js").style.display = "none";
+    document.getElementById("elements_two_js").style.display = "none";
+    document.getElementById("elements_three_js").style.display = "none";
+    document.getElementById("elements_four_js").style.display = "none";
+}
+
+
+
+
+
+
+
+
+		
